9c7715d3c13fd7182568bf74287280dff024fcf6
Rakhat <example@gmail.com>
2023-01-22 21:28:21 +0300

    first commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+﻿"use sctrict"
+//const result = confirm("Are you here?")
+//console.log(result);
+//const answer = +prompt("Vam est 18?");
+//console.log(answer + 5);
+//const answers = [];
+//answers[0] = prompt('как ваше имя?', '');
+//answers[1] = prompt('как ваше фамилия?', '');
+//answers[2] = prompt('сколько вам лет?', '');
+//document.write(answers);
+
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user = 'Ivan';
+alert(`Hello ${user}`);
