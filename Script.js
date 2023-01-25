3a9e5d774b7aff28357b3cf06eb1e4e4bbde7fe0
rahat <rakhat.ismailov.2001@gmail.com>
2023-01-25 19:54:16 +0300

    first lesson

--- Script.js
+++ Script.js
@@ -17,7 +17,7 @@
 
 
 //let incr = 10,
-  //  decr = 10;
+//  decr = 10;
 
 //console.log(incr++);
 //console.log(decr--);
@@ -28,6 +28,31 @@
 //console.log(isChecked && !isClose);
 
 
+
+
+
+
+
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмоы вы посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+  markOfFilm = prompt('На сколько вы  оцените?', ''),
+  a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько вы  оцените?', '');
+
+personalMovieDB.movies[lastFilm] = markOfFilm;
+personalMovieDB.movies[a] = b;
+console.log(personalMovieDB);
+
+
+  
 
 
 
